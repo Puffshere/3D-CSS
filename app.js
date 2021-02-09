536f87ebcf76a90c2aa1f34ef433c473f2dc0150
Shawn Taylor <shawncurtistaylor@gmail.com>
2021-02-09 08:36:38 -0800

    cleans up code

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
 
 container.addEventListener("mousemove", (e) => {
-let xAxis = (window.innerWidth / 2 - e.pageX) /10;
-let yAxis = (window.innerHeight / 2 - e.pageY) /10;
-card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
 container.addEventListener("mouseenter", (e) => {
@@ -24,11 +24,9 @@ container.addEventListener("mouseenter", (e) => {
 container.addEventListener("mouseleave", (e) => {
     card.style.transition = "all 0.5s ease";
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-
     title.style.transform = "translateZ(0px)";
     sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
     description.style.transform = "translateZ(0px)";
     sizes.style.transform = "translateZ(0px)";
     purchase.style.transform = "translateZ(0px)";
-
 })
