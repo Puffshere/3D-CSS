50a56e0ba75200875f6b1e21fea3d49eebc94dc9
Shawn Taylor <shawncurtistaylor@gmail.com>
2021-02-09 23:55:18 -0800

    adds action buttons

--- app.js
+++ app.js
@@ -5,23 +5,32 @@ const logo = document.querySelector(".logo img");
 const login = document.querySelector(".login button");
 const description = document.querySelector(".info h3");
 const roles = document.querySelector(".roles");
+const rolesC = document.querySelector(".active");
+const rolesA = document.querySelector(".a");
+const rolesB = document.querySelector(".b");
+const rolesD = document.querySelector(".d");
+const actions = document.querySelector(".actions");
 
-container.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
 
-container.addEventListener("mouseenter", (e) => {
+function myFunction() {
     card.style.transition = "all 0.5s ease";
     title.style.transform = "translateZ(150px)";
     logo.style.transform = "translateZ(200px) rotateZ(-20deg)";
     description.style.transform = "translateZ(125px)";
     roles.style.transform = "translateZ(100px)";
-    login.style.transform = "translateZ(100px)";
-})
+    rolesC.style.transform = "translateZ(120px)";
+    rolesA.style.transform = "translateZ(100px)";
+    rolesB.style.transform = "translateZ(100px)";
+    rolesD.style.transform = "translateZ(100px)";
+    login.style.transform = "translateZ(175px)";
+    container.addEventListener("mousemove", (e) => {
+        let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+        let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+        card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    });
+}
 
-container.addEventListener("mouseleave", (e) => {
+function myReLoad() {
     card.style.transition = "all 0.5s ease";
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     title.style.transform = "translateZ(0px)";
@@ -29,4 +38,5 @@ container.addEventListener("mouseleave", (e) => {
     description.style.transform = "translateZ(0px)";
     roles.style.transform = "translateZ(0px)";
     login.style.transform = "translateZ(0px)";
-})
+    document.location.reload();
+}
