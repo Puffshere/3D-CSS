be54d73287cddfa3ac55542f7eb55addf526ba3e
Shawn Taylor <shawncurtistaylor@gmail.com>
2021-02-10 15:46:56 -0800

    adds login modal

--- app.js
+++ app.js
@@ -53,3 +53,24 @@ function myReLoad() {
     document.getElementById("hand").setAttribute("style","opacity:1; -moz-opacity:0.5; filter:alpha(opacity=50)");
     document.location.reload();
 }
+
+
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
